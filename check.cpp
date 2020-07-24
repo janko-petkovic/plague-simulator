e8d24af50a4ab6bac055b456ef6e07bccc29807d
PizzaGitHub <67073679+PizzaGitHub@users.noreply.github.com>
2020-07-24 15:17:42 +0200

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,14 +1,26 @@
-#include "utils.cpp"
+#include "Plague.h"
+#include "Plague.cpp"
 #include <iostream>
+#include <armadillo>
 
 using namespace std;
 
 int main() {
-
-
-        vector<float> vector = CumDistrFunc(10,8,1,"Gaussian");
-
-        for (int i=0; i<10; i++) printf ("Probabilità al giorno %i del %.2f \n", i, vector[i]);
-
+		statusChange recovery;
+		recovery.peak = 9;
+		recovery.totProb = 1;
+		recovery.distrType = "Uniform";
+		
+		statusChange death;
+		death.peak = 6;
+		death.totProb = 0.8;
+		death.distrType = "Gaussian";
+		
+		
+		PlagueModel myPlague(10, death, recovery, 0.3);
+		
+		for (int i=0; i<10; i++) printf("Distribuzione guariti giorno %i: %.2f\n",
+		i, myPlague.RecovCumDistr()[i]);
+		
         return 0;
 }

f37e3275d489964aa9a791498205cd7a8e4d4d5c
PizzaGitHub <67073679+PizzaGitHub@users.noreply.github.com>
2020-08-15 22:51:06 +0200

    Delete qsliderfromzero.cpp

--- GUI/qsliderfromzero.cpp
+++
@@ -1,9 +0,0 @@
-#include <qsliderfromzero.h>
-
-QSliderFromZero::QSliderFromZero(QWidget *parent):
-    QSlider(parent) {}
-
-// Given input max set the slider range from 1 to max-1
-void QSliderFromZero::setMaximum(int max) {
-    this->setRange(1,max-1);
-}
